5a262f21e4175298db9008b489b2d1ebedfcae66
215601873@malot.org.il <215601873@malot.org.il>
2025-01-10 15:47:11 +0200

    single  image CSS

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,14 +1,22 @@
 import React, { useContext } from "react";
 import Update from "./Update";
 import Delete from "./Delete";
+import "../css/Photo.css";
 import { PhotoContext } from "./Album";
 function Photo({ photo }) {
     const { updatePhotos, deletePhotos } = useContext(PhotoContext);
     return (
-        <div>
+        // <div>
+        //     <img src={photo.thumbnailUrl} alt={photo.title} />
+        //     <Update item={photo} type='photos' updateDisplay={updatePhotos} />
+        //     <Delete id={photo.id} type='photos' deleteDisplay={deletePhotos} />
+        // </div>
+        <div class="image-container">
             <img src={photo.thumbnailUrl} alt={photo.title} />
+            <div class="overlay-buttons">
             <Update item={photo} type='photos' updateDisplay={updatePhotos} />
             <Delete id={photo.id} type='photos' deleteDisplay={deletePhotos} />
+            </div>
         </div>
     );
 }
